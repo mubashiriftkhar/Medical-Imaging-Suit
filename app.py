5529948b0029fdc8e046848dd09ce9b53feca583
mubashiriftkhar <mubashiriftkhar@gmail.com>
2025-08-22 11:43:36 +0500

    Adding Models

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from torchvision.transforms import transforms
 from PIL import Image
 import io
 
-model=torch.jit.load('Breast_Cancer.pt',map_location=torch.device("cpu"))
-model.eval()
+
 
 
 
@@ -33,21 +32,42 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# ---- Load all models at startup (one time) ----
+models = {
+    "BreastCancer": torch.jit.load("Breast_Cancer.pt", map_location="cpu"),
+    "BrainTumor": torch.jit.load("BrainTumor.pt", map_location="cpu"),
+    "Pneumonia": torch.jit.load("Pneumonia.pt", map_location="cpu"),
+    "KidneyStone": torch.jit.load("KidneyStone.pt", map_location="cpu"),
+}
 
+# Set all models to eval mode
+for model in models.values():
+    model.eval()
 
+# ---- Inference wrapper ----
+def run_inference(model, image, labels):
+    with torch.no_grad():
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+        pred_class = labels[predicted.item()]
+        return pred_class
+labels_map = {
+    "BreastCancer": ["No Cancer Detected", "Cancer Detected"],
+    "BrainTumor": ['glioma','meningioma','notumor','pituitary'],
+    "Pneumonia": ["No Pneumonia Detected", "Pneumonia Detected"],
+    "KidneyStone": ["No Stone Detected", "Stone Detected"],
+}
 @app.post("/upload")
-async def Process(file:UploadFile=File(...)):
+async def Process(file:UploadFile=File(...),Selection="BreastCancer"):
     imageBytes=await file.read()
     image=Image.open(io.BytesIO(imageBytes))
-    image=transform(image).unsqueeze(0)
-    with torch.no_grad():
-         output=model(image)
-         _,predicted=torch.max(output,1)
-         className=[0,1][predicted.item()]
-         if className==0:
-              return "Model does not Detected any Cancer in Brest."
-         elif className==1:
-              return "Model Detected Cancer in Brest."
+    image=transform(image).unsqBreastCancerueeze(0)
+     # Look up the function dynamically
+    if Selection in models:
+        result = run_inference(models[Selection], image, labels_map[Selection])
+    else:
+        result = "Invalid selection."
 
+    return {"result": result}
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
